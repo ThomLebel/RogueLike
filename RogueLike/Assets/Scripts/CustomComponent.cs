325daf0ababd3bb9585672cca5a98a089bc7c65d
ThomLebel <thom.lebel@orange.fr>
2019-12-05 20:40:22 +0100

    CustomComponent.cs

---
+++ RogueLike/Assets/Scripts/CustomComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EventManager))]
+public class CustomComponent : MonoBehaviour
+{
+	[Tooltip("Component name")]
+	public int componentID;
+	[Tooltip("What is this component reacting to ?")]
+	public string[] reactions;
+
+	protected EventManager eventManager;
+
+	protected virtual void Awake()
+	{
+		eventManager = GetComponent<EventManager>();
+	}
+
+	public virtual void ProcessEvent(string id, object value) { }
+}
